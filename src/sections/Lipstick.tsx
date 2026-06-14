f64b69d68709561f4bcee7ef5db33b913e90f68f
Victor <victorm.martinezjr@outlook.com>
2026-06-14 03:06:57 -0400

    feat(ui): create Lipstick section

--- src/sections/Lipstick.tsx
+++ src/sections/Lipstick.tsx
@@ -1,10 +1,30 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap/all";
+
 const Lipstick = () => {
+  useGSAP(() => {
+    gsap.from(".lipstick__img", {
+      scrollTrigger: {
+        trigger: "#lipstick__section",
+        start: "top 50%",
+        toggleActions: "play none none none",
+        scroller: "#main-container",
+      },
+      opacity: 0,
+      y: 50,
+      duration: 0.6,
+      ease: "power2.out",
+    });
+  });
   return (
-    <section className="snap-start h-auto flex flex-col justify-center items-center overflow-hidden px-2 xl:px-0">
+    <section
+      id="lipstick__section"
+      className="snap-start h-auto flex flex-col justify-center items-center overflow-hidden px-2 xl:px-0"
+    >
       <img
         src="assets/lipstick.png"
         alt="lipstick"
-        className="border-2 border-white rounded-2xl"
+        className="lipstick__img border-2 border-white rounded-2xl"
       />
     </section>
   );
